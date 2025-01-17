fd13ee98542658f326c7e0f8d90185ad2982165f
OrnarasUS <ornaras.us@gmail.com>
2025-01-17 13:11:32 +0400

    Удаление ввода TimeRange и создание ввода TimeOnly

--- RegExp.cs
+++ RegExp.cs
@@ -6,7 +6,5 @@ namespace OrnaLibs
     {
         [GeneratedRegex(@"^(?<user>[\w\d-\.]+)@(?<domain>([\w-]+\.)+[\w-]{2,4})$")]
         public static partial Regex Email();
-        [GeneratedRegex(@"^(?<begin>([0-1]\d|2[0-3])\:[0-5]\d)\-(?<end>([0-1]\d|2[0-4])\:[0-5]\d)$")]
-        public static partial Regex TimeRange();
     }
 }
