241a03bf20bd5b1267480c6bcc53afd0f6f12a19
OrnarasUS <ornaras.us@gmail.com>
2025-01-23 12:43:44 +0400

    Создана структура с информацией о приложении

---
+++ Application/Base.cs
@@ -0,0 +1,25 @@
+﻿namespace OrnaLibs;
+
+/// <summary>
+/// Информация о приложении
+/// </summary>
+public partial struct Application
+{
+    /// <summary>
+    /// Регистрация приложения
+    /// </summary>
+    public void Registration()
+    {
+        if (OperatingSystem.IsWindows())
+            RegistrationWindows();
+    }
+
+    /// <summary>
+    /// Удаление записи о приложении
+    /// </summary>
+    public void Unregistration()
+    {
+        if (OperatingSystem.IsWindows())
+            RegistryKeys.LocalMachineUninstallX86.DeleteSubKey(Name);
+    }
+}
---
+++ Application/Windows.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System.Runtime.Versioning;
+
+namespace OrnaLibs;
+
+public partial struct Application
+{
+    [SupportedOSPlatform("windows")]
+    private void RegistrationWindows()
+    {
+        using var reg = RegistryKeys.LocalMachineUninstallX86.CreateSubKey(Name, true);
+        reg.SetValue("Publisher", CompanyName, RegistryValueKind.String);
+        reg.SetValue("EstimatedSize", new FileInfo(AppPath).Length / 1024, RegistryValueKind.DWord);
+        reg.SetValue("InstallLocation", Path.GetDirectoryName(AppPath)!, RegistryValueKind.String);
+        if (Version is { })
+            reg.SetValue("DisplayVersion", Version.ToString(), RegistryValueKind.String);
+        if (!string.IsNullOrWhiteSpace(DisplayName))
+            reg.SetValue("DisplayName", DisplayName, RegistryValueKind.String);
+        if (!string.IsNullOrWhiteSpace(IconPath))
+            reg.SetValue("DisplayIcon", IconPath, RegistryValueKind.String);
+        if (!string.IsNullOrWhiteSpace(Configurator))
+            reg.SetValue("ModifyPath", Configurator, RegistryValueKind.String);
+        if (!string.IsNullOrWhiteSpace(Uninstaller))
+            reg.SetValue("UninstallString", Uninstaller, RegistryValueKind.String);
+    }
+}
